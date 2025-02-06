0172ace7cc3acbe8701b74fbcc7f7eb03f04b10e
Syed Mohd Mehndi <hulk.syed07@gmail.com>
2025-02-06 20:38:02 +0530

    Adding authentication

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create!(
+  email_address: "dummy@gmail.com",
+  password: "dummy123"
+)
